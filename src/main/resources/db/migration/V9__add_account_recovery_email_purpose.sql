9419f7f168bf3b5d9fb1f39098fcd3335f580619
Quasimorphism <bak575755@gmail.com>
2026-09-04 22:41:11 +0900

    탈퇴 계정 복구 정책과 이메일 인증 목적 추가

---
+++ src/main/resources/db/migration/V9__add_account_recovery_email_purpose.sql
@@ -0,0 +1,9 @@
+ALTER TABLE identity_service.email_verification_scopes
+    DROP CONSTRAINT ck_email_verification_scopes_purpose,
+    ADD CONSTRAINT ck_email_verification_scopes_purpose
+        CHECK (purpose IN ('SIGNUP', 'PASSWORD_CHANGE', 'ACCOUNT_RECOVERY'));
+
+ALTER TABLE identity_service.email_verification_challenges
+    DROP CONSTRAINT ck_email_verification_challenges_purpose,
+    ADD CONSTRAINT ck_email_verification_challenges_purpose
+        CHECK (purpose IN ('SIGNUP', 'PASSWORD_CHANGE', 'ACCOUNT_RECOVERY'));
